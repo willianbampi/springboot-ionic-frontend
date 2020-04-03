eab2567b55989f1a82acb94bd50719f897e6fec0
Willian Bampi <willian.bampi@gmail.com>
2020-04-03 12:04:12 -0300

    Get user email of token and save at localStorage.
    Signed-off-by: Willian Bampi <willian.bampi@gmail.com>

--- src/services/authorization.service.ts
+++ src/services/authorization.service.ts
@@ -4,9 +4,12 @@ import { HttpClient } from "@angular/common/http";
 import { API_CONFIGURATION } from "../configurations/api.configuration";
 import { StorageService } from "./storage.service";
 import { LocalUser } from "../models/local_user";
+import { JwtHelper } from "angular2-jwt";
 
 @Injectable()
 export class AuthorizationService {
+
+    jwtHelper: JwtHelper = new JwtHelper();
 
     constructor(public httpClient: HttpClient, public storageService: StorageService){
 
@@ -24,7 +27,8 @@ export class AuthorizationService {
     successfullLogin(authorizationValue : string){
         let authorizationToken = authorizationValue.substring(7);
         let user : LocalUser = {
-            token: authorizationToken
+            token: authorizationToken,
+            email: this.jwtHelper.decodeToken(authorizationToken).sub
         };
         this.storageService.setLocalUser(user);
     }
