7615d89264e63a31d39b0c3b5599b71640034799
Willian Bampi <willian.bampi@gmail.com>
2020-04-04 15:51:39 -0300

    Show details of client page.
    Signed-off-by: Willian Bampi <willian.bampi@gmail.com>

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { StorageService } from '../../services/storage.service';
+import { ClientDTO } from '../../models/client.dto';
+import { ClientService } from '../../services/domain/client.service';
+import { API_CONFIGURATION } from '../../configurations/api.configuration';
 
 @IonicPage()
 @Component({
@@ -9,15 +12,36 @@ import { StorageService } from '../../services/storage.service';
 })
 export class ProfilePage {
 
-  email: string;
+  client: ClientDTO;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public storageService: StorageService) {
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams, 
+    public storageService: StorageService,
+    public clientService: ClientService
+    ) {
   }
 
   ionViewDidLoad() {
     let localUser = this.storageService.getLocalUser();
     if(localUser && localUser.email) {
-      this.email = localUser.email;
+      this.clientService.findByEmail(localUser.email)
+        .subscribe(response => {
+          this.client = response;
+          this.getImageExistis();
+        },
+        error => {});
+    }
+  }
+
+  getImageExistis(){
+    let baseBucketUrl = API_CONFIGURATION.baseBucketUrl;
+    if(baseBucketUrl){
+      this.clientService.getImageFromBucket(this.client.id)
+      .subscribe(response => {
+        this.client.imageUrl = `${baseBucketUrl}/cp${this.client.id}.jpg`;
+      },
+      error => {});
     }
   }
 
---
+++ src/services/domain/client.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Observable } from "rxjs/Rx";
+import { ClientDTO } from "../../models/client.dto";
+import { API_CONFIGURATION } from "../../configurations/api.configuration";
+import { StorageService } from "../storage.service";
+
+@Injectable()
+export class ClientService {
+    
+    constructor(public httpClient: HttpClient, public storageService: StorageService){
+
+    }
+
+    findByEmail(email: string) : Observable<ClientDTO> {
+        let token = this.storageService.getLocalUser().token;
+        let authorizatonHeaders = new HttpHeaders({'Authorization' : 'Bearer ' + token});
+        let url = `${API_CONFIGURATION.baseUrl}/clients/email?email=${email}`;
+        return this.httpClient.get<ClientDTO>(url, {'headers' : authorizatonHeaders});
+    }
+
+    getImageFromBucket(id: string): Observable<any> {
+        let baseBucketUrl = API_CONFIGURATION.baseBucketUrl;
+        if(baseBucketUrl){
+            let url = `${baseBucketUrl}/cp${id}.jpg`;
+            return this.httpClient.get(url, {responseType : 'blob'});
+        }
+    }
+
+}
