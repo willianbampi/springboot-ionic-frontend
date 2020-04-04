14316de42633f0a3d471858d111684f5b9bf4cc9
Willian Bampi <willian.bampi@gmail.com>
2020-04-04 16:34:39 -0300

    Create handle to Error 401, Error 404 and Default.
    Signed-off-by: Willian Bampi <willian.bampi@gmail.com>

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -2,12 +2,14 @@ import { Injectable } from "@angular/core";
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HTTP_INTERCEPTORS } from "@angular/common/http";
 import { Observable } from "rxjs/Rx";
 import { StorageService } from "../services/storage.service";
+import { AlertController } from "ionic-angular";
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 
     constructor(
-        public storageService: StorageService
+        public storageService: StorageService,
+        public alertCtrl: AlertController
     ){
 
     }
@@ -26,17 +28,67 @@ export class ErrorInterceptor implements HttpInterceptor {
             console.log(errorObj);
 
             switch(errorObj.status){
+                case 401:
+                    this.handle401();
+                    break;
                 case 403:
                     this.handle403();
                     break;
+                case 404:
+                    this.handle404();
+                    break;
+                default:
+                    this.handleDefaultError(errorObj);
             }
 
             return Observable.throw(errorObj);
         }) as any;
     }
 
+    handle401() {
+        let alert = this.alertCtrl.create({
+            title: 'Erro 401: falha na autorização',
+            message: 'Email ou senha inválidos',
+            enableBackdropDismiss: false,
+            buttons: [
+                {
+                    text: 'OK'
+                }
+            ]
+        });
+        alert.present();
+    }
+
     handle403() {
         this.storageService.setLocalUser(null);
+    }
+
+    handle404() {
+        let alert = this.alertCtrl.create({
+            title: 'Erro 404: recurso não encontrado',
+            message: 'O recurso solicitado não foi encontrado no servidor',
+            enableBackdropDismiss: false,
+            buttons: [
+                {
+                    text: 'OK'
+                }
+            ]
+        });
+        alert.present();
+    }
+
+    handleDefaultError(error) {
+        let alert = this.alertCtrl.create({
+            title: 'Erro ' + error.status + ': ' + error.error,
+            message: error.message,
+            enableBackdropDismiss: false,
+            buttons: [
+                {
+                    text: 'OK'
+                }
+            ]
+        });
+        alert.present();
     }
 }
 
